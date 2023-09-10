41b41803e9c7d5bebdf2bf15ecedc483a0d9aceb
Vincent <vincent.choquert0@gmail.com>
2023-09-10 22:33:19 +0200

    feat(board-game): request games to api

--- vue-spa/src/pages/DashboardPage.vue
+++ vue-spa/src/pages/DashboardPage.vue
@@ -1,8 +1,28 @@
 <template>
   <h1>Tableau de bord</h1>
+  <div
+      v-for="game in games"
+      class="game-list"
+  >
+    <div>
+      {{ game.gameTitle }}
+    </div>
+  </div>
 </template>
 
 <script setup>
+
+import {onMounted, ref} from "vue";
+import axios from "axios";
+
+const games = ref([]);
+
+onMounted(() => {
+  axios.get("http://127.0.0.1:8000/api/board_games")
+      .then(response => {
+        games.value = response.data["hydra:member"];
+      })
+})
 </script>
 
 <style scoped>
