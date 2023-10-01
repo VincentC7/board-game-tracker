f9d48ef2f112465e6f49c49e7d4d075828397f00
Vincent <vincent.choquert0@gmail.com>
2023-10-01 17:03:31 +0200

    feat(board-game): move table into a component

---
+++ vue-spa/src/components/TableList.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="table">
+    <h1 class="table-title">{{ tableTitle }}</h1>
+    <table>
+      <thead>
+      <tr>
+        <th v-for="header in headers">
+          {{ header }}
+        </th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="game in data">
+        <td v-for="field in fields">
+          {{ game[field] }}
+        </td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  tableTitle: {
+    type: String,
+    required: true,
+  },
+  headers: {
+    type: Array,
+    required: true,
+  },
+  data: {
+    type: Array,
+    required: true,
+  },
+  fields: {
+    type: Array,
+    required: true,
+  },
+});
+</script>
+
+
+<style scoped>
+
+.table {
+  background-color: #ffffff;
+  border-radius: var(--border-radius-meddium);
+  border: 1px solid var(--border-color);
+  overflow: hidden;
+}
+
+.table-title{
+  margin: 1rem;
+}
+
+.table table {
+  width: 100%;
+}
+
+table, tr, td {
+  border: none;
+  border-collapse: collapse;
+}
+
+thead {
+  color: #475569;
+  background-color: #e2e8f0;
+}
+
+thead tr th, tbody tr td {
+  text-align: start;
+  padding: 1rem;
+}
+
+tbody tr:nth-child(odd) {
+  background: #f8f8f8;
+}
+</style>
--- vue-spa/src/pages/DashboardPage.vue
+++ vue-spa/src/pages/DashboardPage.vue
@@ -1,75 +1,39 @@
 <template>
-  <div class="table">
-    <h1 class="table-title">Tableau de bord</h1>
-    <table>
-      <thead>
-      <tr>
-        <th>Titre</th>
-        <th>Date d'achat</th>
-        <th>Prix</th>
-        <th>Note</th>
-      </tr>
-      </thead>
-      <tbody>
-      <tr v-for="game in games">
-        <td>{{ game.gameTitle }}</td>
-        <td>{{ game.purchaseDate }}</td>
-        <td>{{ game.price }}</td>
-        <td>{{ game.rate }}</td>
-      </tr>
-      </tbody>
-    </table>
-  </div>
+  <TableList
+      table-title="Tableau de bord"
+      :headers=headers
+      :data=games
+      :fields=fields
+  >
+  </TableList>
 </template>
 
 <script setup>
 
 import {onMounted, ref} from "vue";
 import axios from "axios";
+import TableList from "@/components/TableList.vue";
 
 const games = ref([]);
+const headers = [
+    "Titre",
+    "Date d'achat",
+    "Prix",
+    "Note"
+]
+
+const fields = [
+  "gameTitle",
+  "purchaseDate",
+  "price",
+  "rate"
+]
 
 onMounted(() => {
   axios.get("http://127.0.0.1:8000/api/board_games")
       .then(response => {
+        console.log(response.data["hydra:member"])
         games.value = response.data["hydra:member"];
       })
 })
 </script>
-
-<style scoped>
-.table {
-  background-color: #ffffff;
-  border-radius: var(--border-radius-meddium);
-  border: 1px solid var(--border-color);
-  overflow: hidden;
-}
-
-.table-title{
-  margin: 1rem;
-}
-
-.table table {
-  width: 100%;
-}
-
-table, tr, td {
-  border: none;
-  border-collapse: collapse;
-}
-
-thead {
-  color: #475569;
-  background-color: #e2e8f0;
-}
-
-thead tr th, tbody tr td {
-  text-align: start;
-  padding: 1rem;
-}
-
-tbody tr:nth-child(odd) {
-  background: #f8f8f8;
-}
-
-</style>
